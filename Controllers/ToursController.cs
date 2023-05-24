a9d3f8743cdcbfc17a9fdc93d44b91975c0161ad
Bobby Griffin <60043676+grifbo@users.noreply.github.com>
2023-05-24 13:15:55 -0400

    Update ToursController.cs

--- Controllers/ToursController.cs
+++ Controllers/ToursController.cs
@@ -28,36 +28,6 @@ namespace IHLA_Template.Controllers
 				t.Date = col["Date"];
 				t.Time = col["Time"];
 
-
-				/*
-				
-				Need a stored proc for returning one if the date matches up
-
-				int intCheckDate = t.CheckDate();
-
-				if(intCheckDate == 1)
-				{
-					Throw("Date already in use");
-				}
-				else
-				{
-					int succesFlag = t.InsertTourDate();
-	
-					TempData["Date"] = t;
-
-					return RedirectToAction("DateScheduled","Date");
-					
-					//I think we can remove the if statement if we can use the successflag as a variable to change the text
-					return RedirectToAction("DateScheduled","Tours", new {SuccessFlag = successFlag,Date = t.Date,Time = t.Time});		
-				}
-
-				return RedirectToAction("DateScheduled", succesFlag);
-				}
-				// IF NULL good to proceed
-				// If !NULL there is already something scheduled
-
-				*/
-
 				int successFlag = t.InsertTourDate();
 
 				return RedirectToAction("DateScheduled","Tours", new {SuccessFlag = successFlag,Date = t.Date,Time = t.Time});
